f7279c98b0a6344bd4a63eadeab417c89d3f36ce
Plugaru Tudor <plugaru.tudor@gmail.com>
2017-11-25 16:53:41 +0200

    stuff made at first day of training

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+
+	title = models.CharField(max_length=255)
+	category = models.CharField(max_length=255)
+	pub_date = models.DateField()
+	content = models.TextField()
+	image_url = models.TextField()
+	image_url_preview = models.TextField()
+
+	def __str__(self):
+		return self.title + ' ' + str(self.pub_date)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # ultimul articol
+    latest_article = Article.objects.all().order_by('-pub_date').first()
+
+    # cele mai recente 4 articole
+    last_4_articles = Article.objects.all().order_by('-pub_date')[:4]
+
+    data = {
+        'latest_article': latest_article,
+        'last_4_articles': last_4_articles
+    }
+    return render(request, 'index.html', data)
+
+
+def about(request):
+    return render(request, 'about.html')
